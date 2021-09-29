e5a469d941e8a68c846424ab41c82acfecd7a920
dinesh <dineshkamble475@gmail.com>
2021-09-29 13:08:09 +0530

    52 product variation

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from carts.models import Cart, CartItem
 from store.models import Product
 from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -13,6 +14,12 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
+    color = request.GET['color']
+    size = request.GET['size']
+    return HttpResponse(color + ' '+ size)
+    exit()
+
+
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
